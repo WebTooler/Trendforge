520d173e11e9e37a4896f4dfbc2b93a7f5e1db4b
WebTooler <singh.scout01@gmail.com>
2026-09-13 08:40:12 +0530

    fix: make robots compatible with static export

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 const base = 'https://webtooler.github.io/Trendforge';
 
 export default function robots(): MetadataRoute.Robots {
