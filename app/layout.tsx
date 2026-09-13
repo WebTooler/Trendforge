1aead4a2b31e7f7bfd50677be2ce539bc4b3bb8f
WebTooler <singh.scout01@gmail.com>
2026-09-13 10:33:05 +0530

    feat: wire optional monetization loader globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
 import { siteMetadata, websiteJsonLd, safeJsonLd } from '@/lib/seo';
+import MonetizationHead from '@/app/components/MonetizationHead';
 
 export const metadata = siteMetadata();
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const jsonLd = safeJsonLd(websiteJsonLd());
-  return <html lang="en"><body><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonLd }} />{children}</body></html>;
+  return <html lang="en"><body><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonLd }} /><MonetizationHead />{children}</body></html>;
 }
